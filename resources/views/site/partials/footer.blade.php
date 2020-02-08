957c6d9e54ecabeed3089549a8e16c9803e3830d
Marcelo Leopold <marceloleob@gmail.com>
2020-02-07 21:36:46 -0500

    Adicionado botao do whatsapp

--- resources/views/site/partials/footer.blade.php
+++ resources/views/site/partials/footer.blade.php
@@ -3,13 +3,15 @@
 	<div class="widget">
 		<img src="#" srcset="style/images/logo-light.png 1x, style/images/logo-light@2x.png 2x" alt="" />
 		<div class="space30"></div>
-		<p>© {!! date('Y') !!} MarceloLeoDEV. All rights reserved.</p>
+		<p><i class="far fa-copyright"></i> {!! date('Y') !!} MarceloLeoDEV. {!! trans('pages/footer.rights') !!}</p>
 		<div class="space10"></div>
 		<ul class="social social-color social-s ml-auto">
+			<li><a href="https://wa.me/17272384933" target="_blank"><i class="jam jam-whatsapp"></i></a></li>
 			<li><a href="https://twitter.com/marceloleob" target="_blank"><i class="jam jam-twitter"></i></a></li>
 			<li><a href="https://www.facebook.com/marceloleob" target="_blank"><i class="jam jam-facebook"></i></a></li>
 			<li><a href="https://www.instagram.com/marceloleob" target="_blank"><i class="jam jam-instagram"></i></a></li>
 			<li><a href="https://www.linkedin.com/in/marcelo-leopold-batista" target="_blank"><i class="jam jam-linkedin"></i></a></li>
 		</ul>
+		<div class="space30"></div>
 	</div>
 </div>
